ba686edd81f97cb6a2b42944533da03c472ff7d9
Remo <remoale@outlook.com>
2025-04-18 14:34:37 -0400

    Database modeling

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,5 +1,6 @@
-import { pgTable, pgEnum, integer, serial, text, timestamp, index, varchar } from "drizzle-orm/pg-core"
+import { pgTable, pgEnum, integer, text, timestamp, uniqueIndex, json, index } from "drizzle-orm/pg-core"
 import { createId } from "@paralleldrive/cuid2"
+import { relations } from 'drizzle-orm'
 
 export const planEnum = pgEnum("plan", ["FREE", "PRO"])
 
@@ -18,9 +19,14 @@ export const user = pgTable(
     createdAt: timestamp("createdAt").defaultNow().notNull(),
     updatedAt: timestamp("updatedAt").defaultNow().notNull(),
 
-    eventCategories: 
   }
 )
+
+export const userRelations = relations(user, ({ many }) => ({
+  eventCategory: many(eventCategory),
+  event: many(event),
+  quota: many(quota)
+}))
 
 export const eventCategory = pgTable(
   "eventCategory",
@@ -30,8 +36,74 @@ export const eventCategory = pgTable(
     color: integer("color"),
     emoji: text("emoji"),
 
-    user: user,
-    userId: text("userId"),
+    userId: text("userId").references(() => user.id).notNull(),
 
-  }
+    createdAt: timestamp("createdAt").defaultNow().notNull(),
+    updatedAt: timestamp("updatedAt").defaultNow().notNull(),
+  },
+  (table) => [
+    uniqueIndex("name_user_id_unique_idx").on(table.name, table.userId),
+  ]
 )
+
+export const eventCategoryRelations = relations(eventCategory, ({ one, many }) => ({
+  userId: one(user, {
+    fields: [eventCategory.userId],
+    references: [user.id],
+  }),
+  events: many(event)
+}))
+
+export const deliveryStatusEnum = pgEnum("deliveryStatus", ["PENDING", "DELIVERED", "FAILED"])
+
+export const event = pgTable(
+  "event",
+  {
+    id: text("id").primaryKey().$defaultFn(() => createId()),
+    formattedMessage: text("formattedMessage"),
+
+    userId: text("userId").references(() => user.id).notNull(),
+
+    name: text("name"),
+    fields: json("fields"),
+
+    deliveryStatus: deliveryStatusEnum().default("PENDING"),
+    
+    createdAt: timestamp("createdAt").defaultNow().notNull(),
+    updatedAt: timestamp("updatedAt").defaultNow().notNull(),
+  },
+  (table) => [
+    index("created_at_idx").on(table.createdAt)
+  ]
+)
+
+export const eventRelations = relations(event, ({ one }) => ({
+  userId: one(user, {
+    fields: [event.userId],
+    references: [user.id],
+  }),
+  eventCategoryId: one(eventCategory, {
+    fields: [event.id],
+    references: [eventCategory.id],
+  })
+}))
+
+export const quota = pgTable(
+  "quota",
+  {
+    id: text("id").primaryKey().$defaultFn(() => createId()),
+    userId: text("userId").references(() => user.id).notNull(),
+
+    year: integer("year"),
+    month: integer("month"),
+    count: integer("count").default(0),
+
+    updatedAt: timestamp("updatedAt").defaultNow().notNull(),
+})
+
+export const quotaRelations = relations(quota, ({ one }) => ({
+  userId: one(user, {
+    fields: [quota.userId],
+    references: [user.id],
+  })
+}))
