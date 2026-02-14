cbb6e80e837a5fd279c02d32a299d2cd7eee39f3
Remo <remoale@icloud.com>
2026-02-14 15:58:20 -0400

    feat: dashboard with stripe integration

--- src/server/routers/auth-router.ts
+++ src/server/routers/auth-router.ts
@@ -1,32 +1,37 @@
-import { currentUser } from "@clerk/nextjs/server";
-import { router } from "../__internals/router";
-import { publicProcedure } from "../procedures";
-import { db } from "@/db";
+import { db } from "@/db"
+import { currentUser } from "@clerk/nextjs/server"
+import { HTTPException } from "hono/http-exception"
+import { router } from "../__internals/router"
+import { publicProcedure } from "../procedures"
+
+export const dynamic = "force-dynamic"
 
 export const authRouter = router({
-    getDatabaseSyncStatus: publicProcedure.query(async ({ c, ctx }) => {
-        const auth = await currentUser()
+  getDatabaseSyncStatus: publicProcedure.query(async ({ c, ctx }) => {
+    const auth = await currentUser()
 
-        if (!auth) {
-            return c.json({ isSynced: false })
-        }
+    if (!auth) {
+      return c.json({ isSynced: false })
+    }
 
-        const user = await db.user.findFirst({
-            where: { externalId: auth.id },
-        })
+    const user = await db.user.findFirst({
+      where: { externalId: auth.id },
+    })
 
-        if (!user) {
-            await db.user.create({
-                data: {
-                    quotaLimit: 100,
-                    email: auth.emailAddresses[0].emailAddress,
-                    externalId: auth.id,
-                },
-            })
+    console.log('USER IN DB:', user);
 
-            return c.json({ isSynced: true })
-        }
+    if (!user) {
+      await db.user.create({
+        data: {
+          quotaLimit: 100,
+          externalId: auth.id,
+          email: auth.emailAddresses[0].emailAddress,
+        },
+      })
+    }
 
-        return c.json({ isSynced: true })
-    }),
+    return c.json({ isSynced: true })
+  }),
 })
+
+// route.ts
